ce498dd22e2f7069f3f6c69c512527fd9c930565
Yael Avramsky <y0583296331@gmail.com>
2025-08-24 19:47:47 +0300

    Resolve merge conflicts after stash pop

--- src/repositories/lesson.repository.ts
+++ src/repositories/lesson.repository.ts
@@ -1,19 +1,4 @@
 import { prisma } from "../db/db";
-export const LessonRepository = {
-    findLessonsByLevelAndStatus: (level: string, status: string, userId: string) =>
-        prisma.lesson.findMany({
-            where: {
-                level: level as any,
-                progresses: {
-                    some: {
-                        userId: userId,
-                        status: status as any,
-                    }
-                }
-            },
-            include: { progresses: true },
-        }),
-};
 
 export const LessonRepository = {
     findLessonsByLevelAndStatus: (level: string, status: string, userId: string) =>
